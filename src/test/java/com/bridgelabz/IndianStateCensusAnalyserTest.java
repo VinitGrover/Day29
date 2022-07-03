f99245e3b2be0a3eb39210b81d4a19c0431505f5
vinitgrover <vinitgrover255@gmail.com>
2022-07-03 16:42:13 +0530

    Added uc1_LoadingFileFromCSV

---
+++ src/test/java/com/bridgelabz/IndianStateCensusAnalyserTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class IndianStateCensusAnalyserTest {
+    
+    private static final String INDIA_CENSUS_CSV_FILE_PATH= "D:\\GitProgram\\Day29\\src\\main\\resources\\IndianStateCensusData.csv";
+
+
+    
+    @Test
+    public void givenIndianCensusDataCSVFileReturnsCorrectRecords()
+    {
+        try
+        {
+            CensusAnalyzer censusAnalyser = new CensusAnalyzer();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            assertEquals(29,numOfRecords);
+        }
+        catch (CensusAnalyserException e) { }
+    }
+}
