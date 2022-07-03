c406188f72573b563e523785f869ba08e14a138b
vinitgrover <vinitgrover255@gmail.com>
2022-07-03 17:12:33 +0530

    Added Test Case 1.2

--- src/test/java/com/bridgelabz/IndianStateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/IndianStateCensusAnalyserTest.java
@@ -1,12 +1,14 @@
 package com.bridgelabz;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class IndianStateCensusAnalyserTest {
     
     private static final String INDIA_CENSUS_CSV_FILE_PATH= "D:\\GitProgram\\Day29\\src\\main\\resources\\IndianStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH= "D:\\GitProgram\\Day2\\src\\main\\resources\\IndianStateCensusData.csv";
 
     @Test
     public void givenIndianCensusDataCSVFileReturnsCorrectRecords()
@@ -21,4 +23,18 @@ public class IndianStateCensusAnalyserTest {
             System.out.println("File not found");
         }
     }
+    @Test
+    public void givenIndianCensusDataCSVFile_whenWithWrongPath_shouldThrowException() {
+        CensusAnalyzer censusAnalyser = new CensusAnalyzer();
+
+        ExpectedException exceptionRule = ExpectedException.none();
+        exceptionRule.expect(CensusAnalyserException.class);
+        try {
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assertions.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
 }
+
